dbd7e429667721f0f60b359fbbeccbaa762d3d88
zeefola <zeefola@gmail.com>
2020-08-18 10:57:31 +0100

    Removed meta tag

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -5,8 +5,8 @@
 <!-- Meta Tags -->
 <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
-<meta name="description" content="MediPress - Health & Medical HTML Template" />
-<meta name="keywords" content="keyword1,keyword2,keyword3,keyword4,keyword5" />
+{{-- <meta name="description" content="MediPress - Health & Medical HTML Template" />
+<meta name="keywords" content="keyword1,keyword2,keyword3,keyword4,keyword5" /> --}}
 <meta name="author" content="ThemeMascot" />
 
 <!-- Page Title -->
